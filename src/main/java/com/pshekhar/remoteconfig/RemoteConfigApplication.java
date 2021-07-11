cce292e6b1fe1e597e3d99f81023e468d9ccf68f
Pratik Shekhar <pratik.shekhar@hyperface.co>
2021-07-12 01:01:15 +0530

    Adding JDBC backend

--- src/main/java/com/pshekhar/remoteconfig/RemoteConfigApplication.java
+++ src/main/java/com/pshekhar/remoteconfig/RemoteConfigApplication.java
@@ -1,12 +1,13 @@
 package com.pshekhar.remoteconfig;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableAutoConfiguration
 public class RemoteConfigApplication {
 
     public static void main(String[] args) {
